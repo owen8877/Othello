3273b33146758f246160be4f2f6042d17fc70156
owen8877 <9379520+owen8877@users.noreply.github.com>
2021-01-06 13:41:33 -0600

    Can handle a few lights interacting.

--- display.cpp
+++ display.cpp
@@ -6,12 +6,14 @@
 #include "shader/shader.h"
 #include <cmath>
 #include <vector>
+#include <boost/filesystem.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-// #include <GL/freeglut.h>
-// #include <GL/glut.h>
-// #include <GL/glu.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+
+#include "stb_image.h"
 
 int screenSize = 80 * BOARD_SIZE, screenWidth = 80 * BOARD_SIZE, screenHeight = 80 * BOARD_SIZE;
 double zoom = DEFAULT_ZOOM;
@@ -410,9 +412,10 @@ void reshape(int width, int height) {
 //     glutPostRedisplay();
 }
 
-struct Vertex_N {
+struct Vertex_NT {
     float position[3];
     float normal[3];
+    float texture[2];
 };
 
 struct Vertex_C {
@@ -429,7 +432,7 @@ struct RenderResource {
 
     RenderResource() = delete;
 
-    RenderResource(const vector<Vertex_N> &vertices, const vector<GLushort> &indices, GLenum mode) : mode{mode} {
+    RenderResource(const vector<Vertex_NT> &vertices, const vector<GLushort> &indices, GLenum mode) : mode{mode} {
         glGenVertexArrays(1, &vao);
         glGenBuffers(1, &vbo);
         glGenBuffers(1, &ibo);
@@ -443,10 +446,12 @@ struct RenderResource {
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices[0]) * indices.size(), &indices[0], GL_STATIC_DRAW);
         drawCount = indices.size();
 
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), nullptr);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), nullptr);
         glEnableVertexAttribArray(0);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *) (3 * sizeof(float)));
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) (3 * sizeof(float)));
         glEnableVertexAttribArray(1);
+        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) (6 * sizeof(float)));
+        glEnableVertexAttribArray(2);
     }
 
     RenderResource(const vector<Vertex_C> &vertices, const vector<GLushort> &indices, GLenum mode) : mode{mode} {
@@ -482,48 +487,48 @@ struct RenderResource {
 };
 
 RenderResource getCube(float cubeSize_2) {
-    const vector<Vertex_N> vertices = {
-            {-0.5f, -0.5f, -0.5f, 0.0f,  0.0f,  -1.0f},
-            {0.5f,  -0.5f, -0.5f, 0.0f,  0.0f,  -1.0f},
-            {0.5f,  0.5f,  -0.5f, 0.0f,  0.0f,  -1.0f},
-            {0.5f,  0.5f,  -0.5f, 0.0f,  0.0f,  -1.0f},
-            {-0.5f, 0.5f,  -0.5f, 0.0f,  0.0f,  -1.0f},
-            {-0.5f, -0.5f, -0.5f, 0.0f,  0.0f,  -1.0f},
+    const vector<Vertex_NT> vertices = {
+            {-0.5f, -0.5f, -0.5f, 0.0f,  0.0f,  -1.0f, 0.0f, 0.0f},
+            {0.5f,  -0.5f, -0.5f, 0.0f,  0.0f,  -1.0f, 1.0f, 0.0f},
+            {0.5f,  0.5f,  -0.5f, 0.0f,  0.0f,  -1.0f, 1.0f, 1.0f},
+            {0.5f,  0.5f,  -0.5f, 0.0f,  0.0f,  -1.0f, 1.0f, 1.0f},
+            {-0.5f, 0.5f,  -0.5f, 0.0f,  0.0f,  -1.0f, 0.0f, 1.0f},
+            {-0.5f, -0.5f, -0.5f, 0.0f,  0.0f,  -1.0f, 0.0f, 0.0f},
 
-            {-0.5f, -0.5f, 0.5f,  0.0f,  0.0f,  1.0f},
-            {0.5f,  -0.5f, 0.5f,  0.0f,  0.0f,  1.0f},
-            {0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f},
-            {0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f},
-            {-0.5f, 0.5f,  0.5f,  0.0f,  0.0f,  1.0f},
-            {-0.5f, -0.5f, 0.5f,  0.0f,  0.0f,  1.0f},
+            {-0.5f, -0.5f, 0.5f,  0.0f,  0.0f,  1.0f,  0.0f, 0.0f},
+            {0.5f,  -0.5f, 0.5f,  0.0f,  0.0f,  1.0f,  1.0f, 0.0f},
+            {0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f, 1.0f},
+            {0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  1.0f, 1.0f},
+            {-0.5f, 0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  0.0f, 1.0f},
+            {-0.5f, -0.5f, 0.5f,  0.0f,  0.0f,  1.0f,  0.0f, 0.0f},
 
-            {-0.5f, 0.5f,  0.5f,  -1.0f, 0.0f,  0.0f},
-            {-0.5f, 0.5f,  -0.5f, -1.0f, 0.0f,  0.0f},
-            {-0.5f, -0.5f, -0.5f, -1.0f, 0.0f,  0.0f},
-            {-0.5f, -0.5f, -0.5f, -1.0f, 0.0f,  0.0f},
-            {-0.5f, -0.5f, 0.5f,  -1.0f, 0.0f,  0.0f},
-            {-0.5f, 0.5f,  0.5f,  -1.0f, 0.0f,  0.0f},
+            {-0.5f, 0.5f,  0.5f,  -1.0f, 0.0f,  0.0f,  0.0f, 0.0f},
+            {-0.5f, 0.5f,  -0.5f, -1.0f, 0.0f,  0.0f,  1.0f, 0.0f},
+            {-0.5f, -0.5f, -0.5f, -1.0f, 0.0f,  0.0f,  1.0f, 1.0f},
+            {-0.5f, -0.5f, -0.5f, -1.0f, 0.0f,  0.0f,  1.0f, 1.0f},
+            {-0.5f, -0.5f, 0.5f,  -1.0f, 0.0f,  0.0f,  0.0f, 1.0f},
+            {-0.5f, 0.5f,  0.5f,  -1.0f, 0.0f,  0.0f,  0.0f, 0.0f},
 
-            {0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f},
-            {0.5f,  0.5f,  -0.5f, 1.0f,  0.0f,  0.0f},
-            {0.5f,  -0.5f, -0.5f, 1.0f,  0.0f,  0.0f},
-            {0.5f,  -0.5f, -0.5f, 1.0f,  0.0f,  0.0f},
-            {0.5f,  -0.5f, 0.5f,  1.0f,  0.0f,  0.0f},
-            {0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f},
+            {0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f},
+            {0.5f,  0.5f,  -0.5f, 1.0f,  0.0f,  0.0f,  1.0f, 0.0f},
+            {0.5f,  -0.5f, -0.5f, 1.0f,  0.0f,  0.0f,  1.0f, 1.0f},
+            {0.5f,  -0.5f, -0.5f, 1.0f,  0.0f,  0.0f,  1.0f, 1.0f},
+            {0.5f,  -0.5f, 0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 1.0f},
+            {0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.0f, 0.0f},
 
-            {-0.5f, -0.5f, -0.5f, 0.0f,  -1.0f, 0.0f},
-            {0.5f,  -0.5f, -0.5f, 0.0f,  -1.0f, 0.0f},
-            {0.5f,  -0.5f, 0.5f,  0.0f,  -1.0f, 0.0f},
-            {0.5f,  -0.5f, 0.5f,  0.0f,  -1.0f, 0.0f},
-            {-0.5f, -0.5f, 0.5f,  0.0f,  -1.0f, 0.0f},
-            {-0.5f, -0.5f, -0.5f, 0.0f,  -1.0f, 0.0f},
+            {-0.5f, -0.5f, -0.5f, 0.0f,  -1.0f, 0.0f,  0.0f, 0.0f},
+            {0.5f,  -0.5f, -0.5f, 0.0f,  -1.0f, 0.0f,  1.0f, 0.0f},
+            {0.5f,  -0.5f, 0.5f,  0.0f,  -1.0f, 0.0f,  1.0f, 1.0f},
+            {0.5f,  -0.5f, 0.5f,  0.0f,  -1.0f, 0.0f,  1.0f, 1.0f},
+            {-0.5f, -0.5f, 0.5f,  0.0f,  -1.0f, 0.0f,  0.0f, 1.0f},
+            {-0.5f, -0.5f, -0.5f, 0.0f,  -1.0f, 0.0f,  0.0f, 0.0f},
 
-            {-0.5f, 0.5f,  -0.5f, 0.0f,  1.0f,  0.0f},
-            {0.5f,  0.5f,  -0.5f, 0.0f,  1.0f,  0.0f},
-            {0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f},
-            {0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f},
-            {-0.5f, 0.5f,  0.5f,  0.0f,  1.0f,  0.0f},
-            {-0.5f, 0.5f,  -0.5f, 0.0f,  1.0f,  0.0f},
+            {-0.5f, 0.5f,  -0.5f, 0.0f,  1.0f,  0.0f,  0.0f, 0.0f},
+            {0.5f,  0.5f,  -0.5f, 0.0f,  1.0f,  0.0f,  1.0f, 0.0f},
+            {0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f},
+            {0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  1.0f, 1.0f},
+            {-0.5f, 0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f},
+            {-0.5f, 0.5f,  -0.5f, 0.0f,  1.0f,  0.0f,  0.0f, 0.0f},
     };
 
     vector<GLushort> indices(36);
@@ -532,6 +537,39 @@ RenderResource getCube(float cubeSize_2) {
     }
 
     return RenderResource(vertices, indices, GL_TRIANGLES); // NOLINT(modernize-return-braced-init-list)
+}
+
+unsigned int loadTexture(char const *path) {
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrComponents;
+    unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+    if (data) {
+        GLenum format;
+        if (nrComponents == 1)
+            format = GL_RED;
+        else if (nrComponents == 3)
+            format = GL_RGB;
+        else if (nrComponents == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(data);
+    } else {
+        std::cout << "Texture failed to load at path: " << path << std::endl;
+        stbi_image_free(data);
+    }
+
+    return textureID;
 }
 
 RenderResource getAxisFrames() {
@@ -641,18 +679,80 @@ void displayThread(const bool *gameEnds) {
     glm::mat4 projMat = glm::perspective(glm::radians(60.0f), (float) screenWidth / (float) screenHeight, 1.0f,
                                          1000.f);
 
-    glm::vec3 lightPos(0.7f, 0.4f, 2.0f);
+    // positions of the point lights
+    glm::vec3 pointLightPositions[] = {
+            glm::vec3(0.7f, 0.2f, 2.0f),
+            glm::vec3(2.3f, -3.3f, -4.0f),
+            glm::vec3(-4.0f, 2.0f, -12.0f),
+            glm::vec3(0.0f, 0.0f, -3.0f),
+    };
     auto lightModelMat = glm::mat4(1.0f);
-    lightModelMat = glm::translate(lightModelMat, lightPos);
+    lightModelMat = glm::translate(lightModelMat, pointLightPositions[0]);
     lightModelMat = glm::scale(lightModelMat, glm::vec3(0.3f));
 
     lightShader.use();
+//    lightShader.setInt("material.diffuse", 0);
+//    lightShader.setInt("material.specular", 1);
+    lightShader.setVec3("viewPos", cameraPos);
+    lightShader.setFloat("material.shininess", 32.0f);
+    // directional light
+    lightShader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
+    lightShader.setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
+    lightShader.setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
+    lightShader.setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
+    // point light 1
+    lightShader.setVec3("pointLights[0].position", pointLightPositions[0]);
+    lightShader.setVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
+    lightShader.setVec3("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
+    lightShader.setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
+    lightShader.setFloat("pointLights[0].constant", 1.0f);
+    lightShader.setFloat("pointLights[0].linear", 0.09);
+    lightShader.setFloat("pointLights[0].quadratic", 0.032);
+    // point light 2
+    lightShader.setVec3("pointLights[1].position", pointLightPositions[1]);
+    lightShader.setVec3("pointLights[1].ambient", 0.05f, 0.05f, 0.05f);
+    lightShader.setVec3("pointLights[1].diffuse", 0.8f, 0.8f, 0.8f);
+    lightShader.setVec3("pointLights[1].specular", 1.0f, 1.0f, 1.0f);
+    lightShader.setFloat("pointLights[1].constant", 1.0f);
+    lightShader.setFloat("pointLights[1].linear", 0.09);
+    lightShader.setFloat("pointLights[1].quadratic", 0.032);
+    // point light 3
+    lightShader.setVec3("pointLights[2].position", pointLightPositions[2]);
+    lightShader.setVec3("pointLights[2].ambient", 0.05f, 0.05f, 0.05f);
+    lightShader.setVec3("pointLights[2].diffuse", 0.8f, 0.8f, 0.8f);
+    lightShader.setVec3("pointLights[2].specular", 1.0f, 1.0f, 1.0f);
+    lightShader.setFloat("pointLights[2].constant", 1.0f);
+    lightShader.setFloat("pointLights[2].linear", 0.09);
+    lightShader.setFloat("pointLights[2].quadratic", 0.032);
+    // point light 4
+    lightShader.setVec3("pointLights[3].position", pointLightPositions[3]);
+    lightShader.setVec3("pointLights[3].ambient", 0.05f, 0.05f, 0.05f);
+    lightShader.setVec3("pointLights[3].diffuse", 0.8f, 0.8f, 0.8f);
+    lightShader.setVec3("pointLights[3].specular", 1.0f, 1.0f, 1.0f);
+    lightShader.setFloat("pointLights[3].constant", 1.0f);
+    lightShader.setFloat("pointLights[3].linear", 0.09);
+    lightShader.setFloat("pointLights[3].quadratic", 0.032);
+    // spotLight
+    lightShader.setVec3("spotLight.position", cameraPos);
+    lightShader.setVec3("spotLight.direction", cameraFocus - cameraPos);
+    lightShader.setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
+    lightShader.setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
+    lightShader.setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
+    lightShader.setFloat("spotLight.constant", 1.0f);
+    lightShader.setFloat("spotLight.linear", 0.09);
+    lightShader.setFloat("spotLight.quadratic", 0.032);
+    lightShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
+    lightShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));
+
     lightShader.setMat4("projection", projMat);
     lightShader.setMat4("view", viewMat);
-    lightShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
-    lightShader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
-    lightShader.setVec3("lightPos", lightPos);
-    lightShader.setVec3("viewPos", cameraPos);
+
+    unsigned int diffuseMap = loadTexture(boost::filesystem::path("resources/textures/container.png").c_str());
+    unsigned int specularMap = loadTexture(boost::filesystem::path("resources/textures/container_specular.png").c_str());
+
+    lightShader.use();
+    lightShader.setInt("material.diffuse", 0);
+    lightShader.setInt("material.specular", 1);
 
     simpleShader.use();
     simpleShader.setMat4("projection", projMat);
@@ -681,6 +781,14 @@ void displayThread(const bool *gameEnds) {
         lightShader.use();
         modelMat = glm::rotate(modelMat, (float) M_PI / 4 / FPS, glm::vec3(0.0f, 1.0f, 0.0f));
         lightShader.setMat4("model", modelMat);
+
+        // bind diffuse map
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, diffuseMap);
+        // bind specular map
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, specularMap);
+
         cube.draw();
 
         simpleShader.use();
--- shader/simple.vert
+++ shader/simple.vert
@@ -2,6 +2,7 @@
 
 layout (location = 0) in vec3 aPosition;
 layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec2 aTexCoords;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -9,9 +10,11 @@ uniform mat4 projection;
 
 out vec3 normal;
 out vec3 fragPos;
+out vec2 texCoords;
 
 void main() {
     gl_Position = projection * view * model * vec4(aPosition, 1.0);
     normal = mat3(transpose(inverse(model))) * aNormal;
     fragPos = vec3(model * vec4(aPosition, 1.0));
+    texCoords = aTexCoords;
 }
